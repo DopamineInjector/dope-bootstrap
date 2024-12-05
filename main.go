9e458241cfb986f853d9bd14abd4d321fed7d809
tpalinski <tpalinski@icloud.com>
2024-12-05 10:55:32 +0100

    [deploy] fix net interface binding

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	addr := flag.String("address", "127.0.0.1", "Ip address to the bootstrap server")
+	addr := flag.String("address", "0.0.0.0", "Ip address to the bootstrap server")
 	port := flag.Int("port", 7312, "Port to run boostrap server on")
 	flag.Parse()
 
